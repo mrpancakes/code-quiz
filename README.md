b53512efaf59e06ad2e802461b4019eeb0adfa24
Scott Strittmatter <scott.a.strittmatter@gmail.com>
2021-02-28 15:58:29 -0600

    Created questions array, event listener for the Start Quiz button, and started on the getQuestion function.

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # homework-04-code-quiz
+
+* When Start button is clicked:
+    * The Main section is hidden
+    * The timer function is called
+    * A random quiz question appears
+
+
+* When a question is answered:
+    * If correct, next question appears & 10 pts are added to your score
+    * If wrong, timer decrements 10 seconds, no pts added to your score
--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,81 @@
 let timerEl = document.querySelector("#timer");
+let homeScreen = document.querySelector("#home-screen");
+let startButton = document.querySelector("#start-quiz");
+let quizQuestions = document.querySelector("#quiz");
 
-let timeLeft = 200; // Starting amount of time when quiz begins
+let timeLeft = 199; // Starting amount of time when quiz begins
+let currentQuestionIndex = 0;
 
+startButton.addEventListener("click", startQuiz); // Event listener for Start Quiz buttons
 
-
+// Countdown timer function
 function countdown(){
    let timeInterval = setInterval(function(){
         if (timeLeft > 1) {
-            timerEl.textContent = timeLeft + " seconds";
+            timerEl.textContent = `${timeLeft} seconds`;
             timeLeft--;
         } else if (timeLeft === 1){
-            timerEl.textContent = timeleft + " second";
-            time--;
+            timerEl.textContent = `${timeLeft} second`;
+            timeLeft--;
         } else {
+            timerEl.innerHTML = "<strong>0 seconds</strong>";
             clearInterval(timeInterval);
             // call some other function, maybe the "game over" function
         }
    }, 1000)
 }
+
+// Use this to cycle through and display the questions with answers. Should hide the "main" section of the page and show the quiz questions
+function startQuiz(){
+    homeScreen.setAttribute("class", "hidden"); // Hides the homescreen main paragraph
+    quizQuestions.setAttribute("class", "visible"); // Makes the quiz section of the page visible
+
+    countdown(); 
+
+    getQuestion ();
+} 
+
+
+function getQuestion(){
+    let currentQuestion = questions[currentQuestionIndex];
+    let titleEl = document.querySelector("#question-title");
+    let questionEl = document.querySelector("#choices");
+    titleEl.textContent = currentQuestion.title;
+    questionEl.textContent = currentQuestion.choices;
+
+}
+
+
+
+// Array of questions/answers
+let questions = [
+    {
+        title: "What's the proper format to initalize an array?",
+        choices: ["let myArr = ();", "let myArr = [];", "let myArr = {};", "let myArr() = ;"],
+        answer: "let myArr = [];"
+    },
+    {
+        title: "To deterime hwo many characters are in a string, you add ______ to the end of your string/variable.",
+        choices: [".characterCounter", ".length()", ".string", ".length"],
+        answer: ".length"
+    },
+    {
+        title: "How do you convert 'SCOTT' to 'scott'?",
+        choices: [".toLowerCase()", ".lower", ".toLowercase", ".lowercase()"],
+        answer: ".toLowerCase()"
+    },
+    {
+        title: "How do you convert a number to a string?",
+        choices: ["number + 0", "number.toString()", "number.string", "number.createString()"],
+        answer: "number.toString()"
+    },
+    {
+        title: "How do you divide 10 by 5 in JavaScript?",
+        choices: ["10 / 5", "5 / 10", "10 % 5", "10 // 5"],
+        answer: "10 / 5"
+    }
+]
+
+
+
+
